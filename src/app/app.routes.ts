ffbdf7360c110e440fb6ad395fba642b4e3e9e55
juanRCoder <guillecarck@gmail.com>
2024-03-20 17:19:03 -0500

    feat: add new categorys

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AnimeMangaComponent } from './anime-manga/anime-manga.component';
+import { CienciasComponent } from './ciencias/ciencias.component';
+import { DeportesComponent } from './deportes/deportes.component';
+import { GeneralComponent } from './general/general.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '', component: DashboardComponent },
   { path: 'anime-manga', component: AnimeMangaComponent },
+  { path: 'ciencias', component: CienciasComponent },
+  { path: 'deportes', component: DeportesComponent },
+  { path: 'general', component: GeneralComponent },
 ];
